dd00ea609bda09d271b20033c4331bc5805f842d
Gashaw-ambaw <gashawambaw77@gmail.com>
2025-12-22 20:01:27 +0200

    work on Index2D.java only left equals func

---
+++ src/assignments/Ex2/Index2D.java
@@ -0,0 +1,48 @@
+package assignments.Ex2;
+
+public class Index2D implements Pixel2D {
+    private int _x;
+    private int _y;
+
+    public Index2D(int w, int h) {
+       this._x = w;
+       this._y = h;
+    }
+    public Index2D(Pixel2D other) {
+        this._x =  other.getX();
+        this._y =  other.getY();
+    }
+    @Override
+    public int getX() {;
+        return _x ;
+    }
+
+    @Override
+    public int getY() {
+
+        return _y;
+    }
+
+    @Override
+    public double distance2D(Pixel2D p2) {
+
+        double dx = this._x - p2.getX();
+        double dy = this._y - p2.getY();
+
+        return  Math.sqrt(dx * dx + dy * dy);
+    }
+
+    @Override
+    public String toString() {
+        String  point=  "(" + _x + "," + _y + ")";
+        return  point;
+    }
+
+    @Override
+    public boolean equals(Object p) {
+        boolean ans = true;
+
+
+        return ans;
+    }
+}
